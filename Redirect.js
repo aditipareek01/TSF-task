78e95364aaa345f33cacc6488766a4cede2acc14
aditipareek01 <136443615+aditipareek01@users.noreply.github.com>
2023-09-21 15:05:57 +0530

    Add files via upload

---
+++ Redirect.js
@@ -0,0 +1,15 @@
+function redirectToPayment() {
+    window.location.href = "/Payment.html";
+  }
+  
+  function dntMoney() {
+    window.open(
+      "https://pages.razorpay.com/pl_GFF2rkynv03jik/view",
+      "mywin",
+      "width=569, height=696"
+    );
+  }
+  
+  function backHome() {
+    window.location.href = "/Index.html";
+  }
